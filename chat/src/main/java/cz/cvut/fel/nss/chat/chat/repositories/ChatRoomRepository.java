dc4293f12000497b67da462f5e9f17a33388f519
Tobias Le <letobias@fel.cvut.cz>
2024-04-25 15:58:18 +0000

    Update 3 files
    - /chat/src/main/java/cz/cvut/fel/nss/chat/chat/entities/ChatLog.java
    - /chat/src/main/java/cz/cvut/fel/nss/chat/chat/entities/ChatRoom.java
    - /chat/src/main/java/cz/cvut/fel/nss/chat/chat/repositories/ChatRoomRepository.java

--- chat/src/main/java/cz/cvut/fel/nss/chat/chat/repositories/ChatRoomRepository.java
+++ chat/src/main/java/cz/cvut/fel/nss/chat/chat/repositories/ChatRoomRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, Integer> {
 }
