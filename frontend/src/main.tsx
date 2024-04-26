c353747fafdc39de999fc22604569156f3a9d776
Jakub Najman <jakub@najman.cz>
2024-04-26 22:43:25 +0200

    fixed receiving messages twice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,8 @@
 window.global = window;
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
 		<App />
-	</React.StrictMode>
 );
