d62c9c61b9f516b231d2af5c4abd9429963bbb01
Tobias Le <letobias@fel.cvut.cz>
2024-04-26 11:52:02 +0200

    fixed pagination lmao

--- chat/src/main/java/cz/cvut/fel/nss/chat/chat/repositories/ChatMessageRepository.java
+++ chat/src/main/java/cz/cvut/fel/nss/chat/chat/repositories/ChatMessageRepository.java
@@ -1,18 +1,18 @@
 package cz.cvut.fel.nss.chat.chat.repositories;
 
 import cz.cvut.fel.nss.chat.chat.entities.ChatMessage;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Highlight;
 import org.springframework.data.elasticsearch.annotations.HighlightField;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends ElasticsearchRepository<ChatMessage, String> {
     @Highlight(fields = {
             @HighlightField(name = "content")
     })
-    List<ChatMessage> findAllByMessageLogIdOrderByTimestampInSeconds(Integer messageLogId, Pageable pageable);
+    Page<ChatMessage> findAllByMessageLogIdOrderByTimestampInSeconds(Integer messageLogId, Pageable pageable);
 }
